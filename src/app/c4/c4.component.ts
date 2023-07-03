993ade4d6f5c0192cb213287db23c01f0ff43370
cecilioAlvarez <cmac@arquitecturajava.com>
2023-07-03 10:21:47 +0200

    ngfor

---
+++ src/app/c4/c4.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-c4',
+  templateUrl: './c4.component.html',
+  styleUrls: ['./c4.component.css']
+})
+export class C4Component {
+
+  lista:string[]=["hola","que","tal","estas"];
+
+}
