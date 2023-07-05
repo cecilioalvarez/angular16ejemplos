ec8699571034182f73ae546c0b723069dbebf3b3
cecilioAlvarez <cmac@arquitecturajava.com>
2023-07-05 11:02:05 +0200

    select con objeto

--- src/app/c10/c10.component.ts
+++ src/app/c10/c10.component.ts
@@ -10,9 +10,15 @@ export class C10Component {
 
   personas:Persona[]=[];
 
+  personaSeleccionada:Persona ={} as Persona;
   constructor() {
 
     this.personas.push(new Persona("pedro","perez",20),new Persona("angel","sanchez",30),new Persona("ana","gomez",40))
+  }
+
+  seleccionar(persona:Persona) {
+    console.log(persona);
+    this.personaSeleccionada=persona;
   }
   
 
