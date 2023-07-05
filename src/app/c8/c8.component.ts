9227b0fa888b638e67a2dd8f5dca2f0dc9472018
cecilioAlvarez <cmac@arquitecturajava.com>
2023-07-05 09:48:39 +0200

    gestion evento lenguaje

--- src/app/c8/c8.component.ts
+++ src/app/c8/c8.component.ts
@@ -7,8 +7,8 @@ import { Component } from '@angular/core';
 })
 export class C8Component {
 
-  cambiar():void {
+  cambiar(valor:string):void {
 
-    console.log("hola desde el grupo");
+    console.log(valor);
   }
 }
