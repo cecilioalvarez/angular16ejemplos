652f37199150ad96cd08faa3d2ae49f7f9380092
cecilioAlvarez <cmac@arquitecturajava.com>
2023-07-07 09:00:05 +0200

    fechaNacimiento

--- src/app/persona.ts
+++ src/app/persona.ts
@@ -1,6 +1,6 @@
 export class Persona {
     //constructor class expresion
-    constructor(public nombre:string,public apellidos:string,public edad:number) {
+    constructor(public nombre:string,public apellidos:string,public edad:number,public fechaNacimiento?:Date) {
 
     }
 }
