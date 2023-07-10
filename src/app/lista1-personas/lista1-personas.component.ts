4c5452444f0f61fb4600b2c0a35852d2fdd40fc6
cecilioAlvarez <cmac@arquitecturajava.com>
2023-07-10 09:46:11 +0200

    datasource

--- src/app/lista1-personas/lista1-personas.component.ts
+++ src/app/lista1-personas/lista1-personas.component.ts
@@ -1,38 +1,50 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Persona } from '../persona';
 import { PersonaService } from '../persona.service';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-lista1-personas',
   templateUrl: './lista1-personas.component.html',
   styleUrls: ['./lista1-personas.component.css']
 })
-export class Lista1PersonasComponent {
+export class Lista1PersonasComponent implements OnInit {
 
-  estado:string="listar";
+  estado: string = "listar";
   //un conjunto de variales asociadas a la vista y poco mas.
-  
-  displayedColumns: string[] = ['nombre', 'apellidos', 'edad','botonBorrar'];
-  dataSource;
 
-  personas:Persona[]=[];
-  personaNueva:Persona= {} as Persona;
+  displayedColumns: string[] = ['nombre', 'apellidos', 'edad', 'botonBorrar'];
+  dataSource = new MatTableDataSource<Persona>();
 
-  constructor(public personaService:PersonaService) {
+  personas: Persona[] = [];
+  personaNueva: Persona = {} as Persona;
 
-    this.personas=personaService.buscarTodos();
-    this.dataSource = this.personas;
+  constructor(public personaService: PersonaService, private changeDetectorRefs: ChangeDetectorRef) {
+
+    this.personas = personaService.buscarTodos();
+    this.dataSource.data = this.personas;
+    
   }
-  insertar(){
+  ngOnInit(): void {
+   
+  }
+  insertar() {
 
     this.personaService.insertar(this.personaNueva);
-    this.estado='listar';
+
+    this.estado = 'listar';
+
+    this.dataSource= new MatTableDataSource(this.personaService.buscarTodos());
   }
 
   verFormularioInsertar() {
-    this.estado='formularioInsertar';
+    this.estado = 'formularioInsertar';
   }
-  borrar(persona:Persona) {
+  borrar(persona: Persona) {
+
     this.personaService.borrar(persona);
+    this.dataSource.data = this.personaService.buscarTodos();
+
+
   }
 }
--- src/app/persona.service.ts
+++ src/app/persona.service.ts
@@ -14,6 +14,7 @@ export class PersonaService {
   }
 
   public buscarTodos():Persona[] {
+    console.log(this.personas);
     return this.personas;
   }
 
